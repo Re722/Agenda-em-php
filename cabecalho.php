872a2fce7eb7a270093841912b395a45ec8ce018
DESKTOP-I7RV353\renaldo <sao1barbara@gmail.com>
2021-08-10 21:51:15 -0300

    initial commit

---
+++ cabecalho.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>CRUD Contato - PHP Profissional</title>
+<link href="./resources/lib/bootstrap/css/css/bootstrap.css" rel="stylesheet">
+<link href="./resources/lib/bootstrap/fonts"> 
+
+    <body>
+    <div class="container">
+    <div class="jumbotron">
+    <h1>CRUD - Contato - PHP Profissional</h1>
+    </div>
+    
+   
